2e145a21b8c0cf753888a3512e6f50011cc98527
綾田陽介 <202x102x@stu.kobe-u.ac.jp>
2020-05-28 01:10:11 +0900

    modify W05

--- W05/main.js
+++ W05/main.js
@@ -1,4 +1,4 @@
-function main()
+function gouraud()
 {
     var width = 500;
     var height = 500;
@@ -45,7 +45,7 @@ function main()
     }
 }
 
-function main2()
+function phong()
 {
     var width = 500;
     var height = 500;
@@ -91,4 +91,195 @@ function main2()
         torus_knot.rotation.y += 0.01;
         renderer.render( scene, camera );
     }
+}
+
+function blinn_phong()
+{
+    var width = 500;
+    var height = 500;
+
+    var scene = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 5 );
+    scene.add( camera );
+
+    var light = new THREE.PointLight();
+    light.position.set( 5, 5, 5 );
+    scene.add( light );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+    var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
+    var material = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        vertexShader: document.getElementById('blinn_phong.vert').text,
+        fragmentShader: document.getElementById('blinn_phong.frag').text,
+        uniforms: {
+            light_position: {type: "v3", value: light.position},
+        }
+    });
+
+    var torus_knot = new THREE.Mesh( geometry, material );
+    scene.add( torus_knot );
+
+    loop();
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        torus_knot.rotation.x += 0.01;
+        torus_knot.rotation.y += 0.01;
+        renderer.render( scene, camera );
+    }
+}
+
+function cook_torrance()
+{
+    var width = 500;
+    var height = 500;
+
+    var scene = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 5 );
+    scene.add( camera );
+
+    var light = new THREE.PointLight();
+    light.position.set( 5, 5, 5 );
+    scene.add( light );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+    var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
+    var material = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        vertexShader: document.getElementById('cook_torrance.vert').text,
+        fragmentShader: document.getElementById('cook_torrance.frag').text,
+        uniforms: {
+            light_position: {type: "v3", value: light.position},
+        }
+    });
+
+    var torus_knot = new THREE.Mesh( geometry, material );
+    scene.add( torus_knot );
+
+    loop();
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        torus_knot.rotation.x += 0.01;
+        torus_knot.rotation.y += 0.01;
+        renderer.render( scene, camera );
+    }
+}
+
+function toon()
+{
+    var width = 500;
+    var height = 500;
+
+    var scene = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 5 );
+    scene.add( camera );
+
+    var light = new THREE.PointLight();
+    light.position.set( 5, 5, 5 );
+    scene.add( light );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+    var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
+    var material = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        vertexShader: document.getElementById('toon.vert').text,
+        fragmentShader: document.getElementById('toon.frag').text,
+        uniforms: {
+            light_position: {type: "v3", value: light.position},
+        }
+    });
+
+    var torus_knot = new THREE.Mesh( geometry, material );
+    scene.add( torus_knot );
+
+    loop();
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        torus_knot.rotation.x += 0.01;
+        torus_knot.rotation.y += 0.01;
+        renderer.render( scene, camera );
+    }
+}
+
+function test(){
+    var width = 500;
+    var height = 500;
+
+    var scene = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 5 );
+    scene.add( camera );
+
+    var light = new THREE.PointLight();
+    light.position.set( 5, 5, 5 );
+    scene.add( light );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+
+    var mesh;
+    // 画像を読み込む
+    var loader = new THREE.TextureLoader();
+    loader.load('ita.jpg', function(texture) {
+        var material = new THREE.MeshBasicMaterial( {
+            color: 0xffffff,
+            map:texture
+        } );
+        var geometry = new THREE.CubeGeometry(2, 2, 2);
+        mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+            
+        loop();
+    });
+    
+    
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.01;
+        renderer.render( scene, camera );
+    }
+    
 }
