23bdecff5429031c149caf72f98dd243e1dd33fd
綾田陽介 <202x102x@stu.kobe-u.ac.jp>
2020-05-29 20:02:20 +0900

    test toon

--- W05/main.js
+++ W05/main.js
@@ -239,6 +239,7 @@ function test(){
     var height = 500;
 
     var scene = new THREE.Scene();
+    var sceneEdge = new THREE.Scene();
 
     var fov = 45;
     var aspect = width / height;
@@ -247,29 +248,55 @@ function test(){
     var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
     camera.position.set( 0, 0, 5 );
     scene.add( camera );
+    sceneEdge.add( camera );
 
     var light = new THREE.PointLight();
     light.position.set( 5, 5, 5 );
     scene.add( light );
+    sceneEdge.add( light );
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( width, height );
     document.body.appendChild( renderer.domElement );
 
 
-    var mesh;
+    var mesh, meshEdge;
     // 画像を読み込む
     var loader = new THREE.TextureLoader();
-    loader.load('ita.jpg', function(texture) {
-        var material = new THREE.MeshBasicMaterial( {
-            color: 0xffffff,
-            map:texture
+    loader.load('toon.png', function(texture) {
+        var material = new THREE.ShaderMaterial( {
+            uniforms: {
+                vertexColors: THREE.VertexColors,
+                vertexShader: document.getElementById('toon.vert').text,
+                fragmentShader: document.getElementById('toon.frag').text,
+                edgeColor: {type: 'v4',value: new THREE.Vector4(0, 0, 0, 0)},
+                edge: {type: 'i',value: true},
+                light_position: {type: "v3", value: light.position},
+                texture: {type: 't',value: texture},
+            }
         } );
-        var geometry = new THREE.CubeGeometry(2, 2, 2);
+        // var geometry = new THREE.TorusGeometry(1, 0.5, 8, 16);
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
         mesh = new THREE.Mesh(geometry, material);
+        meshEdge = new THREE.Mesh(geometry, material);
         scene.add(mesh);
+        sceneEdge.add(meshEdge);
+
+        //render front face.
+        meshEdge.material.side = THREE.FrontSide;
+        mesh.material.uniforms.edgeColor.value = new THREE.Vector4(0, 0, 0, 0);
+        mesh.material.uniforms.edge.value = false;
+        mesh.rotation.x += 1;
+        mesh.rotation.y += 1;
+        renderer.render(scene, camera);
+
+        //render back face as edge.
+        meshEdge.material.side = THREE.BackSide;
+        meshEdge.material.uniforms.edgeColor.value = new THREE.Vector4(0, 0, 0, 1);
+        meshEdge.material.uniforms.edge.value = true;
+        renderer.render(sceneEdge, camera);
             
-        loop();
+        // loop();
     });
     
     
